8e0c1181a6cc7c6a8c508ae540f0e1415220b57f
Gabriel <suppershock777@gmail.com>
2024-09-01 12:06:59 -0300

    Criado nossa migration

---
+++ database/migrations/2024_08_31_160659_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            // id do registro, autoincrement e primary key
+            $table->id();
+
+            // Nome do porduto (name) - string
+
+            // Código do produto - string - não pode repetir (code)
+
+            // quantidade do produto - int - por default quantidade = 0
+
+            $table->timestamps(); // quando o registro foi criado e quanto foi atualizado
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
